83d949f3b1e7c71c5b69d51e8800c9ded19b2308
dingxiaohuan <wydingxiaohuan@163.com>
2022-02-28 14:20:12 +0800

    路由切换缓存优化

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# wrap-route
-对react-route中路由组件包装
+
+keep 包装后的组件 KeepRoute 必须传一个 location 属性，该属性的值为项目中用到的最外层 router 中的 location 才行，最外层 router 组件 HashRouter 或 BrowserRouter
+
+如果不想使用路由缓存使用无须使用 keep 即可
+
+```jsx
+import React from 'react';
+import { Route ,withRouter} from 'react-router';
+import keep from 'wrapRoute';
+
+// 通过 withRouter 拿到 router 中的 location
+const KeepRoute = withRouter(keep(Route))
+
+
+<KeepRoute  exact path="/test" component={() => {}}/>
+```
---
+++ src/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import matchPath from './matchPath'
+
+
+
+export const KeepContext = React.createContext(null)
+
+export default function keep(Route) {
+  return class extends React.Component {
+    state = {
+      match: null,
+      pageRoot: null
+    }
+    static propTypes = {
+      location: PropTypes.object.isRequired
+    }
+    static getDerivedStateFromProps({ location, ...rest }) {
+      return {
+        match: matchPath(location.pathname, rest)
+      }
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+      if (nextProps.isKeepDisabled) return true
+      const shouldUpdate = Boolean(nextState.match);
+      this.rootSwitch(shouldUpdate);
+      if (shouldUpdate) {
+        const pageRoot = this.state.pageRoot;
+        if (pageRoot && pageRoot.style.display === 'none') {
+          return false;
+        }
+      }
+      return shouldUpdate
+
+    }
+    rootSwitch(shouldUpdate) {
+      const dom = this.state.pageRoot
+      if (dom) {
+        if (shouldUpdate) {
+          const display = dom.style.display;
+          if (display === 'none') {
+            dom.style.display = 'block'
+          }
+        } else {
+          dom.style.display = 'none'
+        }
+      }
+    }
+    savePageRoot = (pageRoot) => {
+      if (pageRoot) {
+        this.setState({ pageRoot })
+      }
+    }
+    render() {
+      const { match, pageRoot } = this.state;
+      return match ? <div ref={this.savePageRoot} style={{ height: '100%' }}>
+        <KeepContext.Provider value={{ match, pageRoot }}>
+          {<Route {...this.props} />}
+        </KeepContext.Provider>
+      </div> : null
+    }
+  }
+}
