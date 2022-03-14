a5d6c58ba758ca04a36ea15cad9330541f9c1015
dingxiaohuan <wydingxiaohuan@163.com>
2022-03-14 11:25:54 +0800

    feat: 新增路由 onPageShow && onPageHide 生命周期

--- src/index.js
+++ src/index.js
@@ -1,75 +1,109 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import matchPath from './matchPath'
+import React from 'react';
+import PropTypes, { symbol } from 'prop-types';
+import matchPath from './matchPath';
 
-
-
-export const KeepContext = React.createContext(null)
+export const KeepContext = React.createContext(null);
 
 export default function keep(Route) {
+  const NORMAL = 'NORMAL';
+  const SHOW = 'SHOW';
+  const HIDE = 'HIDE';
   return class extends React.Component {
+    showPageCallbacks = [];
+    hidePageCallbacks = [];
+    routePhase = NORMAL;
     state = {
-      match: null,
-      pageRoot: null
-    }
+      matched: null,
+      pageRoot: null,
+    };
     static propTypes = {
       location: PropTypes.object.isRequired,
-      shouldKeep: PropTypes.func
-    }
+      shouldKeep: PropTypes.func,
+    };
     static getDerivedStateFromProps({ location, ...rest }) {
       return {
-        match: matchPath(location.pathname, rest)
-      }
+        matched: matchPath(location.pathname, rest),
+      };
     }
     shouldComponentUpdate(nextProps, nextState) {
       let isKeep = true;
+      const pageRoot = this.state.pageRoot;
       const { shouldKeep, location, ...rest } = nextProps;
       if (typeof shouldKeep === 'function') {
         isKeep = shouldKeep({
-          root: this.state.pageRoot,
-          matcher: function (pathname) {
-            return matchPath(pathname, rest)
-          }, path: nextProps.path
-        })
+          root: pageRoot,
+          match: function (pathname) {
+            return matchPath(pathname, rest);
+          },
+          path: nextProps.path,
+        });
       }
       if (!isKeep) {
         return true;
       }
-      const shouldUpdate = Boolean(nextState.match);
-      this.rootSwitch(shouldUpdate);
-      if (shouldUpdate) {
-        const pageRoot = this.state.pageRoot;
-        if (pageRoot && pageRoot.style.display === 'none') {
-          return false;
-        }
-      }
-      return shouldUpdate
-    }
-    rootSwitch(shouldUpdate) {
-      const dom = this.state.pageRoot
-      if (dom) {
-        if (shouldUpdate) {
-          const display = dom.style.display;
-          if (display === 'none') {
-            dom.style.display = 'block'
-          }
-        } else {
-          dom.style.display = 'none'
-        }
-      }
-    }
-    savePageRoot = (pageRoot) => {
+      let shouldUpdate = Boolean(nextState.matched);
       if (pageRoot) {
-        this.setState({ pageRoot })
+        if (
+          this.routePhase === NORMAL ||
+          (!this.state.matched && shouldUpdate && this.routePhase === HIDE)
+        ) {
+          this.routePhase = SHOW;
+          pageRoot.style.display = 'block';
+          shouldUpdate = false;
+          this.showPageCallbacks.forEach(callback => callback.call(null));
+        } else if (
+          this.state.matched &&
+          !shouldUpdate &&
+          this.routePhase === SHOW
+        ) {
+          this.routePhase = HIDE;
+          pageRoot.style.display = 'none';
+          this.hidePageCallbacks.forEach(callback => callback.call(null));
+        }
       }
+      return shouldUpdate;
     }
+    savePageRoot = pageRoot => {
+      if (pageRoot) {
+        this.setState({ pageRoot });
+      }
+    };
+    onPageShow = (callback, isImmediate = true) => {
+      if (
+        typeof callback === 'function' &&
+        this.showPageCallbacks.indexOf(callback) === -1
+      ) {
+        this.showPageCallbacks.push(callback);
+      }
+      if (isImmediate && this.routePhase === SHOW) {
+        this.showPageCallbacks.forEach(callback => callback.call(null));
+      }
+    };
+    onPageHide = callback => {
+      if (typeof callback === 'function') {
+        this.hidePageCallbacks.push(callback);
+      }
+    };
     render() {
-      const { match, pageRoot } = this.state;
-      return match ? <div ref={this.savePageRoot} style={{ height: '100%' }}>
-        <KeepContext.Provider value={{ match, pageRoot }}>
-          {<Route {...this.props} />}
-        </KeepContext.Provider>
-      </div> : null
+      const { matched, pageRoot } = this.state;
+      return matched ? (
+        <div
+          className={this.props.path}
+          ref={this.savePageRoot}
+          style={{ height: '100%' }}
+        >
+          <KeepContext.Provider
+            value={{
+              matched,
+              pageRoot,
+              onPageShow: this.onPageShow,
+              onPageHide: this.onPageHide,
+            }}
+          >
+            {<Route {...this.props} />}
+          </KeepContext.Provider>
+        </div>
+      ) : null;
     }
-  }
+  };
 }
